d55d9063c48b6e80c64be3a78f3190de5d7578f8
ApxuTechTop <apxutech@gmail.com>
2026-07-22 11:25:53 +0700

    change base path for imports

--- vite.config.ts
+++ vite.config.ts
@@ -40,6 +40,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: "/samosbor/map/",
   css: {
     preprocessorOptions: {
       scss: {
